daac9e0e093e2358e34a6afff98e9cbd0adf4240
lucia.campeglia <lulacampeglia@gmail.com>
2022-08-01 23:49:50 -0300

    first commit

---
+++ cart.js
@@ -0,0 +1,136 @@
+let cart;
+
+//Local Storage
+if(localStorage.getItem('cart')){
+    cart = JSON.parse(localStorage.getItem('cart'))
+} else{
+    localStorage.setItem('cart', JSON.stringify([]))
+    cart = JSON.parse(localStorage.getItem('cart'))
+}
+
+//Sumar cantidad productos + precio
+const totalCart = () => {
+    return cart.reduce((acc, product) => acc + product.price * product.quantity, 0);
+}
+
+//Tabla carrito compra
+const body = document.getElementById('cartMain');
+function showCart() {
+    if(cart.length == 0){
+        const text = `
+        <div class='cartContainer'>
+            <h1 class='txtCart mt-3'> El carrito está vacío </h1>
+            <a class='btnBack' href='products.html'>
+                <button>Volver</button>
+            </a>
+        </div>
+        `;
+        body.innerHTML += text;
+    } else{
+        const titulo = `
+            <div class= 'cartContainer'>
+                <h1 class='txtCart text-center mt-3'> Carrito de compras </h1>
+            </div>`;
+        body.innerHTML += titulo;
+        const table = `
+        <div class= 'tableContainer'>
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th class='txtTable'>PRODUCTOS</th>
+                        <th class='txtTable'>CANTIDAD</th>
+                        <th class='txtTable'>PRECIO</th>
+                    </tr>  
+                </thead>
+                <tbody id="tBody">
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th></th> 
+                        <th></th>
+                        <th class='txtTotal'>Total:</th> 
+                        <td id='total'>$${totalCart().toLocaleString()}</td>
+                    </tr>
+                </tfoot>
+            </table>
+            </div>
+            <div class='btn-container text-center'>
+                <button class='btnCancel'>VACIAR CARRITO</button>
+            </div>
+            <div class='btn-container text-center'>
+                <button class='btnFinish'>TERMINAR COMPRA</button>
+            </div>`;
+            
+            body.innerHTML += table;
+    
+            const tbody = document.getElementById('tBody')
+            for (let i = 0; i < cart.length; i++) {
+                const element = cart[i];
+                const {id, name, img, price, quantity} = element;
+                const cartCards = `
+                <tr id=${id}>
+                    <th>
+                    <button class='btn btn-danger btnDelete'>
+                        <img src="./Imágenes/trash.png" alt="icono-trash" width="20px">                        
+                    </button>
+                    </th>
+                    <th class='tableDetails'><img class='imgProductCart'src=${img} alt='Imagen-producto><spam class='productName' width="70px">${name}</span></th>
+                    <th>${quantity}</th>
+                    <th>$${(quantity*price).toLocaleString()}</th>
+                </tr>
+                `
+                tbody.innerHTML += cartCards;
+                
+            }
+            // const btnCancel = document.getElementsByClassName('btnCancel');
+
+            // //Vaciar carrito
+            // btnCancel[0].onclick = () =>{
+            //     cart.length = 0;
+            //     const totalPrice = document.getElementById('total');
+            //     totalPrice.innerText = "";
+            //     cartCounter.innerText = (cart.length);
+            //     localStorage.setItem('cart',[]);
+            //     actualizarCarrito();
+            // }
+    } 
+}
+
+showCart();
+
+// Actualizar carrito
+function actualizarCarrito (){
+    const counter = document.getElementById('cartCounter')
+    counter.innerHTML = cart.length
+}
+
+//Eliminar producto carrito
+
+const deleteProduct = (prodId) => {
+    const product = cart.find((product) => product.id === prodId)
+    const indice = cart.indexOf(product)
+    cart.splice(indice,1)
+    localStorage.setItem('cart', JSON.stringify(cart));
+    deleteProductCart();
+    actualizarCarrito();
+}
+let deleteProductCart = document.getElementsByClassName('btnDelete');
+deleteProductCart.onclick = deleteProduct
+
+//Sweet Alert
+const finishingPurchase = () => {
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Su compra ha sido procesada con éxito',
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
+let btnFinish = document.getElementsByClassName('btnFinish');
+btnFinish[0].onclick = finishingPurchase;
+
+
+
+totalCart();
---
+++ products.js
@@ -0,0 +1,189 @@
+let products = [
+    {
+        "id":"001",
+        "name": "Cárdigan Merlín (Gris)",
+        "color": "Gris",
+        "price": 6200,
+        "img": "Imágenes/Hombre/cardigan-merlin-1.jpg",
+    },
+    {
+        "id":"002",
+        "name": "Cárdigan Merlín (Azúl)",
+        "color": "Azúl",
+        "price": 6200,
+        "img": "Imágenes/Hombre/cardigan-merlin-2.jpg",
+    },
+    {
+        "id":"003",
+        "name": "Cárdigan Merlín (Bordó)",
+        "color": "Bordó",
+        "price": 6200,
+        "img": "Imágenes/Hombre/cardigan-merlin-3.jpg",
+    },
+    {
+        "id":"004",
+        "name": "Remera Lucas (Gris)",
+        "color": "Gris",
+        "price": 3900,
+        "img": "Imágenes/Hombre/remera-lucas-1.jpg",
+    },
+    {
+        "id":"005",
+        "name": "Remera Lucas (Verde)",
+        "color": "Verde",
+        "price": 3900,
+        "img": "Imágenes/Hombre/remera-lucas-2.jpg",
+    },
+    {
+        "id":"006",
+        "name": "Remera Lucas (Azúl)",
+        "color": "Azúl",
+        "price": 3900,
+        "img": "Imágenes/Hombre/remera-lucas-3.jpg",
+    },
+    {
+        "id":"007",
+        "name": "Sweater Mercurio (Celeste)",
+        "color": "Celeste",
+        "price": 5800,
+        "img": "Imágenes/Hombre/sweater-mercurio-1.jpg",
+    },
+    {
+        "id":"008",
+        "name": "Sweater Mercurio (Azúl)",
+        "color": "Azúl",
+        "price": 5800,
+        "img": "Imágenes/Hombre/sweater-mercurio-2.jpg",
+    },
+    {
+        "id":"009",
+        "name": "Sweater Mercurio (Bordó)",
+        "color": "Bordó",
+        "price": 5800,
+        "img": "Imágenes/Hombre/sweater-mercurio-3.jpg",
+    },
+    {
+        "id":"010",
+        "name": "Sweater Pedro (Gris claro)",
+        "color": "Gris claro",
+        "price": 5700,
+        "img": "Imágenes/Hombre/sweater-pedro-1.jpg",
+    },
+    {
+        "id":"011",
+        "name": "Sweater Pedro (Celeste)",
+        "color": "Celeste",
+        "price": 5700,
+        "img": "Imágenes/Hombre/sweater-pedro-2.jpg",
+    },
+    {
+        "id":"012",
+        "name": "Sweater Pedro (Gris oscuro)",
+        "color": "Gris oscuro",
+        "price": 5700,
+        "img": "Imágenes/Hombre/sweater-pedro-3.jpg",
+    },
+    
+]
+
+let cart;
+
+//Local Storage
+if(JSON.parse(localStorage.getItem('cart'))){
+    cart = JSON.parse(localStorage.getItem('cart'))
+} else{
+    localStorage.setItem('cart', JSON.stringify([]))
+    cart = JSON.parse(localStorage.getItem('cart'))
+}
+
+// DOM - Catálogo productos
+function setProducts(){
+    for (let i = 0; i < products.length; i++) {
+        const element = products[i];
+        const {id, name, color, price, img } = element
+        const card = `
+        <div id='gridContainer' class='products m-3'>
+                <p class="text-center my-1">${name}</p>
+            <div class="text-center mt-2">
+                <img class= "imgProduct" src= ${img} alt= "Imagen-producto" width="380px"/>
+            </div>
+            <div>
+                <p class="text-center mt-2">$${price.toLocaleString()}</p>
+            </div>
+            <div class="btn-container btn-add text-center my-2">
+                <button id=${id} class="btnAdd">Agregar a carrito</button>
+            </div>
+        </div>  
+           
+        `
+        const container = document.getElementById('container')
+        container.innerHTML += card
+    }
+}
+
+setProducts();
+
+//Agregar producto al carrito
+const btnAdd = document.getElementsByClassName('btnAdd')
+
+for (let i = 0; i < btnAdd.length; i++) {
+    const element = btnAdd[i];
+    element.addEventListener('click', addToCart)
+}
+//Sweet Alert
+const sweetAlert = () => {
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Producto agregado al carrito',
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
+
+let btnAdding = document.getElementsByClassName('btnAdd');
+btnAdding.onclick = sweetAlert;
+
+sweetAlert();
+
+//Traer data del producto 
+function addToCart(e){2
+    const btn = e.target;
+    const idBtn = btn.getAttribute ('id')
+    const findedProduct = products.find(product => product.id == idBtn)
+    const inCart = cart.find(product => product.id == findedProduct.id)
+    console.log (inCart)
+    
+    if(!inCart){
+        cart.push({...findedProduct, quantity: 1})
+    }else{
+        let cartFilter = cart.filter(product => product.id != inCart.id)
+        cart = [...cartFilter, {...inCart, quantity: inCart.quantity + 1}]
+    }
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
+
+
+const counter = document.getElementById('cartCounter')
+counter.innerHTML = cart.length
+
+
+//Buscador
+/* const searchingSweater = products.filter (sweater => 
+    sweater.name == "Sweater" ||sweater.name == "sweater" ||sweater.name == "sueter")
+    let sweaterSearch = document.getElementById('btnAdd');
+    sweaterSearch.onclick = searchingSweater */
+
+let filter = document.addEventListener("keyup", e=>{
+    if (e.target.matches("#searcher")){
+    document.querySelectorAll(".products").forEach(prenda => {
+        prenda.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+        ?prenda.classList.remove("filtro")
+        :prenda.classList.add("filtro")
+    })
+    }
+})
+    
+addToCart();
+filter();
+    
